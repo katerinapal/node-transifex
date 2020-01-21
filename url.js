e0de399897dfe00de93e52e380f30349baaf06b0
katerina <katpaltoglou@gmail.com>
2020-01-21 10:45:19 +0200

    update branch

--- url.js
+++ url.js
@@ -1,10 +1,13 @@
-var exportedObject = function ( projectName ) {
-  const BASE_URL = "https://www.transifex.com/api/2/";
-  const BASEP_URL = BASE_URL + "project/";
-  const projectUrl = BASEP_URL + projectName + "/";
-  const prSlug = BASEP_URL + "<project_slug>/resource/<resource_slug>/";
-  const plSlug = BASEP_URL + "<project_slug>/language/<language_code>/";
-  const translationUrlBase = prSlug + "translation/<language_code>/";
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var exportedObject = function exportedObject(projectName) {
+  var BASE_URL = "https://www.transifex.com/api/2/";
+  var BASEP_URL = BASE_URL + "project/";
+  var projectUrl = BASEP_URL + projectName + "/";
+  var prSlug = BASEP_URL + "<project_slug>/resource/<resource_slug>/";
+  var plSlug = BASEP_URL + "<project_slug>/language/<language_code>/";
+  var translationUrlBase = prSlug + "translation/<language_code>/";
 
   var API = {
     projectInstanceAPI: BASEP_URL + "<project_slug>/?details",
@@ -16,7 +19,7 @@ var exportedObject = function ( projectName ) {
     languageInstanceURL: plSlug + "?details",
     contributorForURL: plSlug + "<type>/",
     translationUrlBase: prSlug + "translation/<language_code>/",
-    translationMethodURL:  translationUrlBase + "?file",
+    translationMethodURL: translationUrlBase + "?file",
     translationStringsPutURL: translationUrlBase + "strings/",
     translationStringsURL: translationUrlBase + "strings/?details<string_key>",
     statsMethodURL: prSlug + "stats/<language_code>/",
@@ -31,4 +34,5 @@ var exportedObject = function ( projectName ) {
   };
 };
 
-export { exportedObject as urljs };;
+exports.urljs = exportedObject;
+;
